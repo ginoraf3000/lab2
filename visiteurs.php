e6739bc1d681c795399d1b24b1c847e7a58124ae
ginoraf3000 <ginoraf3000@hotmail.com>
2013-09-03 11:18:26 -0400

    tableau fait 2

--- visiteurs.php
+++ visiteurs.php
@@ -1,13 +1,1 @@
-<?php
-    header('Content-Type: text/html; charset=utf-8');
-    $pays = fopen("pays.txt", "r");
-
-    $i = 1;
-    while(!feof($pays))
-    {
-        $tableau[$i] = fgets($pays);
-        echo $tableau[$i]."<br>";
-        $i++;
-    }
-    fclose($pays);
-?>
+/var/http/visiteurs.php

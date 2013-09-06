8475fad2578ee22605a7d298719a150ad7c389df
ginoraf3000 <ginoraf3000@hotmail.com>
2013-09-06 16:40:40 -0400

    version du 6 sept

--- visiteurs.php
+++ visiteurs.php
@@ -1,1 +1,26 @@
-/var/http/visiteurs.php
+<html>
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    </head>
+    <body>
+    <table border="3">
+        <?php
+
+        echo '<img src="http://tinyurl.com/ktougt4" />
+        <br>
+        <a href="http://tinyurl.com/ktougt4"> Source</a>';
+        $pays = fopen("pays.txt", "r");
+        $i = 1;
+        while(!feof($pays))
+        {
+            $tableau[$i] = fgets($pays);
+            echo "<tr><td>".$tableau[$i]."</td></tr>";
+            $i++;
+        }
+        fclose($pays);
+        ?>
+    </table>
+    </body>
+</html>
+
+

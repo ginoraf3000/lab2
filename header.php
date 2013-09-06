6c3855aa650a0bd076dc7de95e994d2eb9c09156
ginoraf3000 <ginoraf3000@hotmail.com>
2013-09-06 13:34:02 -0400

    Header et 1er lien

--- header.php
+++ header.php
@@ -1,8 +1,14 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: administrateur
- * Date: 13-09-06
- * Time: 17:19
- * To change this template use File | Settings | File Templates.
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Bienvenue</title>
+</head>
+
+<body>
+    <h1>Bienvenue sur le site de Jean-Raphaël Doyon !</h1>
+    <h2>Bonne route</h2>
+    <p>Voici le plan du site : <a href="lab1a.php">Lab 1</a></p>
+
+</body>
+</html>

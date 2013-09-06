7fdd1801734c878f36e7f6906ddf46709c45ab79
ginoraf3000 <ginoraf3000@hotmail.com>
2013-09-06 16:37:20 -0400

    instauration du login presque fini? 3

--- secret.php
+++ secret.php
@@ -10,9 +10,22 @@ setcookie("sexe",$sexe,time()+$expire);
 <head>
     <meta charset="utf-8" />
     <title>Ceci est un secret ;)</title>
+    <style>p {color:#ff0000;}</style>
 </head>
 
 <body>
-    <img src="bienvenue-1.jpg">
+    <?php
+    if($_COOKIE["prenom"] or $_COOKIE["sexe"])
+    {
+        echo '<img src="bienvenue-1.jpg">';
+
+    }
+    else
+    {
+        echo "<p>Impossible d’accéder à cette page, vous devez être logué!</p>";
+    }
+
+    ?>
+    <button type="button" title="retour" onclick="self.location.href='login.php'">Retour</button>
 </body>
 </html>

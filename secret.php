7e89909af688f0fb6728d0ade3386ef6f7869230
ginoraf3000 <ginoraf3000@hotmail.com>
2013-09-06 15:15:29 -0400

    instauration du login 4

--- secret.php
+++ secret.php
@@ -1,8 +1,17 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: administrateur
- * Date: 13-09-06
- * Time: 14:21
- * To change this template use File | Settings | File Templates.
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Titre</title>
+</head>
+
+<body>
+    <?
+    $expire = 365*24*3600;
+    $nom = $_REQUEST["nom"];
+    $sexe = $_REQUEST["sexe"];
+    setcookie("Biscuit",$nom,$sexe,time()+$expire);
+    ?>
+    <p>Bite</p>
+</body>
+</html>

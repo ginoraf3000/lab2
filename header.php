043544cfb581a4438da53d1ab97607a77095ffae
ginoraf3000 <ginoraf3000@hotmail.com>
2013-09-06 13:40:14 -0400

    Header et tous les liens

--- header.php
+++ header.php
@@ -8,7 +8,12 @@
 <body>
     <h1>Bienvenue sur le site de Jean-Raphaël Doyon !</h1>
     <h2>Bonne route</h2>
-    <p>Voici le plan du site : <a href="lab1a.php">Lab 1</a></p>
+    <p>Voici le plan du site : <br>
+        <a href="lab1a.php">Lab 1a</a><br>
+        <a href="lab1b.php">Lab 1b</a><br>
+        <a href="lab1c.php">Lab 1c</a><br>
+        <a href="visiteurs.php">Lab 2</a><br>
+    </p>
 
 </body>
 </html>
